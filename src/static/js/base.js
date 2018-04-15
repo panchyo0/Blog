d4c4c2a39d998cdd20dd5acada0efca83afe83b0
pang <383066079@qq.com>
2018-04-15 16:53:26 -0600

    fix ajax bug change detail ui

--- src/static/js/base.js
+++ src/static/js/base.js
@@ -28,7 +28,7 @@ $(function() {
 
     $.ajax({
         type: "GET",
-        url: "",
+        url: "ajax",
         data: {
             'search_by' : $('#search').val(),
             'csrfmiddlewaretoken' : $("input[name=csrfmiddlewaretoken]").val()
