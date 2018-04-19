693e0139f562769f2857fa02a60f7376b914139f
pang <383066079@qq.com>
2018-04-18 23:17:46 -0600

    side bar jq done

---
+++ src/static/js/postList.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $( ".thumbnail" ).hover(
+  function() {
+    var content=$( this ).find('#post-content');
+    var shadow=$( this ).find('.no-shadow');
+    content.css({"display":"block"});
+    shadow.addClass("with-shadow");
+  }, function() {
+    var content=$( this ).find('#post-content');
+    var shadow=$( this ).find('.no-shadow');
+    content.css({"display":"none"});
+    shadow.removeClass("with-shadow");
+  }
+  );
+});
