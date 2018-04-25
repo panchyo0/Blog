8eb7bbdefbf2cfb3be7d5000f4ede86b60d954d6
pang <383066079@qq.com>
2018-04-24 23:05:36 -0600

    add media screen for recent post and home page

--- src/static/js/postList.js
+++ src/static/js/postList.js
@@ -2,14 +2,24 @@
 $(document).ready(function(){
   $( ".thumbnail" ).hover(
   function() {
-    var content=$( this ).find('#post-content');
+    var content=$( this ).find('#article-content');
+    var title=$( this ).find('#article-title');
+    var date=$( this ).find('#article-date');
     var shadow=$( this ).find('.no-shadow');
+
     content.css({"display":"block"});
+    title.css({"display":"none"});
+    date.css({"display":"none"});
     shadow.addClass("with-shadow");
   }, function() {
-    var content=$( this ).find('#post-content');
+    var content=$( this ).find('#article-content');
+    var title=$( this ).find('#article-title');
+    var date=$( this ).find('#article-date');
     var shadow=$( this ).find('.no-shadow');
+
     content.css({"display":"none"});
+    title.css({"display":"block"});
+    date.css({"display":"block"});
     shadow.removeClass("with-shadow");
   }
   );
